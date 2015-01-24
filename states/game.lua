2205d4f3d85ddd4b0531bb7ccba4323f3da796b2
Vijay Pemmaraju <vijayvmp@gmail.com>
2015-01-23 20:14:51 -0500

    added new files

---
+++ states/game.lua
@@ -0,0 +1,39 @@
+require 'states.state'
+cam = require 'lib.hump.camera'
+
+camera = cam()
+
+
+function game:initialize()
+  
+end
+
+function game:init()
+end
+
+
+function game:enter(previous) -- run every time the state is entered
+  state.enter(self,previous)
+  TEsound.playLooping("assets/sounds/Running in the 90's.mp3")
+  
+end
+
+function game:update(dt)
+  
+  
+  
+end
+
+function game:draw()
+  state:draw()
+  camera:attach()
+  love.graphics.setColor(255,255,255,255)
+  love.graphics.rectangle('fill', 50, 40, 100, 100);
+  camera:detach()
+end
+
+function game:mousepressed(x,y, btn)
+end
+ 
+     
+return game
